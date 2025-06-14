e86d5fb6af3decd6732fd951fc08dbdd083d4fa9
Sarmad <sarmadkhalique001@gmail.com>
2025-06-14 14:44:04 +0500

    feat: add search bar and assets

---
+++ components/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div className="search">
+            <div>
+                <img src="./search.svg" alt="Search" />
+                <input type="text" placeholder="Search for a movie" />
+            </div>
+        </div>
+    )
+}
+
+export default Search
